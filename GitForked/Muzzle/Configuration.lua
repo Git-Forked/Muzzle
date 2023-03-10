4541c1221ad03fc34a06581e2db37aa7298e5e76
Git-Forked <66106762+Git-Forked@users.noreply.github.com>
2023-03-10 13:56:09 -0500

    Add files via upload

---
+++ GitForked/Muzzle/Configuration.lua
@@ -0,0 +1,9 @@
+-- Muzzle (Configuration.lua)
+
+Set = {}
+
+-- Configuration Options:
+
+-- Verbose (default is: false) [Set to true for debugging.]
+-- Shows who was muzzled, the block reason, and the message content.
+Set.Verbose = false
